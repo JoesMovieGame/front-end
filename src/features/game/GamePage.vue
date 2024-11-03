0401ce9ca0e17cedc4bb32e6227a0d8ebfb475f0
Joe Burdick <joseph.a.burdick@gmail.com>
2024-11-03 14:25:49 -0800

    Add TailwindCSS

--- src/features/game/GamePage.vue
+++ src/features/game/GamePage.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-import MovieGuesser from './MovieGuesser.vue';
-import MovieGuessResults from './MovieGuessResults.vue';
+import MovieGuesser from "./MovieGuesser.vue";
+import MovieGuessResults from "./MovieGuessResults.vue";
 </script>
+
 <template>
-  <div>
-    <h1>Play</h1>
-    <movie-guess-results />
-    <movie-guesser />
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div class="bg-white p-8 rounded shadow-md text-center">
+      <h1 class="text-2xl font-bold mb-4">Welcome to my Movie Game</h1>
+      <movie-guess-results />
+      <movie-guesser />
+    </div>
   </div>
 </template>
